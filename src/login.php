d69c11da48d74db1887f2fde981005651f755e8b
LuisEd-dev <luiseduardo123321@gmail.com>
2020-02-06 12:23:09 -0300

    Criado: login conectado a db

---
+++ src/login.php
@@ -0,0 +1,24 @@
+<?php
+    //destroy
+    if($_SERVER['REQUEST_METHOD'] !== 'POST'){
+        header('Location: ../login.html');
+    }
+    $user = $_POST["user"];
+    $pass = $_POST["pass"];
+    if($user != "" AND $pass != ""){
+        $pdo = new PDO("mysql:host=localhost;dbname=adminsistema", "root", "toor"); 
+        $consulta = $pdo->prepare('SELECT * FROM admin WHERE usuario = :usuario AND senha = :senha');
+        $consulta->bindParam(':usuario', md5($user));
+        $consulta->bindParam(':senha', md5($pass));
+        $consulta->execute();
+        if ($consulta->fetch(PDO::FETCH_ASSOC) <= 0){
+            header('Location: ../index.html?failedLogin');
+        } else {
+            session_start();
+            $sess = $_COOKIE["PHPSESSID"];
+            header('Location: ../painel.php?id='.$sess);
+        }
+    } else {
+        header('Location: ../index.html?verificacao');
+    }
+?>
